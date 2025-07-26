41fd5ee25e00b1c275a67474ed85d6252faa86c6
SonaalKalra <sonaal.kalra23@outlook.com>
2025-07-26 11:19:33 +0530

    first commit

---
+++ backed.tf
@@ -0,0 +1,23 @@
+terraform {
+    required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.25.0"
+    }
+  }
+#  backend "azurerm" {
+#     subscription_id      = "84382787-1367-4937-befb-9757f44087ef"
+#     resource_group_name  = "example-test-rg"  
+#     storage_account_name = "examplestoracc01" 
+#     container_name       = "tfstate0011"      
+#     key                  = "test001.tfstate"  
+#   }
+}
+
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+  subscription_id = "84382787-1367-4937-befb-9757f44087ef"
+}
+
---
+++ main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_resource_group" "testexamplerg" {
+  name     = "example-test-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "rg01" {
+  name                     = "examplestoracc01"
+  resource_group_name      = azurerm_resource_group.testexamplerg.name
+  location                 = azurerm_resource_group.testexamplerg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  
+}
+resource "azurerm_storage_account" "testexamplestorage" {
+  name                     = "examplestoracc01"
+  resource_group_name      = azurerm_resource_group.testexamplerg.name
+  location                 = azurerm_resource_group.testexamplerg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+resource "azurerm_storage_container" "testexamplecontainer" {
+  name                  = "tfstate0011"
+  storage_account_id    = azurerm_storage_account.testexamplestorage.id
+  container_access_type = "private"
+}
+
+resource "azurerm_resource_group" "testexamplerg01" {
+  name     = "example-test-rg01"
+  location = "East US"
+}
