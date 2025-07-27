fa04c9ad6c59d0c3aa74f43977ecb7dd56743e79
SonaalKalra <sonaal.kalra23@outlook.com>
2025-07-27 12:43:14 +0530

    4th commit

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
 resource "azurerm_resource_group" "testexamplerg" {
-  name     = "example-test-rg"
+  name     = "exampledata-test-rg01"
   location = "East US"
 }
 
-resource "azurerm_storage_account" "rg01" {
-  name                     = "examplestoracc01"
-  resource_group_name      = azurerm_resource_group.testexamplerg.name
-  location                 = azurerm_resource_group.testexamplerg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  
-}
+
 resource "azurerm_storage_account" "testexamplestorage" {
   name                     = "databolexamplestorage1"
   resource_group_name      = azurerm_resource_group.testexamplerg.name
@@ -24,7 +17,3 @@ resource "azurerm_storage_container" "testexamplecontainer" {
   container_access_type = "private"
 }
 
-resource "azurerm_resource_group" "testexamplerg01" {
-  name     = "example-test-rg01"
-  location = "East US"
-}
