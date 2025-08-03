7f66c85da6e9abaa76b97514c2361b33fe7f707f
SonaalKalra <sonaal.kalra23@outlook.com>
2025-08-03 14:00:47 +0530

    6th commit

--- backed.tf
+++ backed.tf
@@ -8,7 +8,7 @@ terraform {
 #  backend "azurerm" {
 #     subscription_id      = "84382787-1367-4937-befb-9757f44087ef"
 #     resource_group_name  = "example-test-rg"  
-#     storage_account_name = "examplestoracc01" 
+#     storage_account_name = "databolexamplestorage1" 
 #     container_name       = "tfstate0011"      
 #     key                  = "test001.tfstate"  
 #   }
